3ad402727f04c6fcd5a0d22f3e27fa254dc5326c
realkevinkwon <kevinkwonmail@gmail.com>
2023-03-17 02:07:38 -0400

    Create App Engine service

---
+++ web-server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from App Engine!");
+});
+
+// Listen to the App Engine-specified port.
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
