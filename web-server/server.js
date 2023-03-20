b141d8389b713943f4a36c57ca107922b8b86a5c
realkevinkwon <kevinkwonmail@gmail.com>
2023-03-20 11:35:20 -0400

    convert indent to 2 spaces

--- web-server/server.js
+++ web-server/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from App Engine!");
+  res.send("Hello from App Engine!");
 });
 
 // Listen to the App Engine-specified port.
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });
