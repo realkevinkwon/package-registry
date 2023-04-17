abb1699fc41c12425a9c7a79a3098a34b28cbd29
realkevinkwon <kevinkwonmail@gmail.com>
2023-04-17 13:58:43 -0400

    create basic menubar and dashboard

--- web-server/src/App.js
+++ web-server/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import MenuBar from './components/MenuBar/MenuBar';
+import Dashboard from './components/Dashboard/Dashboard';
+
+import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NextUIProvider>
+      <MenuBar />
+      <Dashboard />
+    </NextUIProvider>
   );
 }
 
