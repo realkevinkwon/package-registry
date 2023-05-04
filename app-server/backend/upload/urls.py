ca7852c252b4e1da944a266e558d4aadf9886cd0
realkevinkwon <kevinkwonmail@gmail.com>
2023-05-03 22:27:55 -0400

    file view

--- app-server/backend/upload/urls.py
+++ app-server/backend/upload/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import upload_file, download_file, file_list, file_view, indv_package, indv_package_rate
+from .views import upload_file, download_file, file_list, file_view#, indv_package, indv_package_rate
 
 urlpatterns = [
     # Path to view all files in the bucket
     path('upload', upload_file, name='upload_file'),
     path('packages/', file_list, name='file_list'),
-    path('file_view/', file_view, name='file_view'),
+    path('file_view/<filename>', file_view, name='file_view'),
     # path('download-file/', download_file, name='download_file'),
     # path(r"^upload/(?P<slug>[\w-]+)$",indv_package, name = 'individual-packages'), #change upload to package /package/{id}
     # path(r"^upload/(?P<slug>[\w-]+)/rate$",indv_package_rate, name = 'individual-ratings'), #change upload to package /package/{id}/rate
